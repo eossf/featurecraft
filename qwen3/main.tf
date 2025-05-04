6b5dfaf476df26a5f70d3c6ddd5d27f8b7731487
metairie <stephane.metairie@gmail.com>
2025-05-04 11:07:39 +0200

    fix redundant upload + new prompt

--- qwen3/main.tf
+++ qwen3/main.tf
@@ -233,3 +233,21 @@ resource "null_resource" "upload_compose_file" {
     destination = "/root/docker-compose.yaml"
   }
 }
+
+# launch kestra using docker compose
+resource "null_resource" "launch_kestra" {
+  depends_on = [null_resource.install_docker_with_ansible, local_file.compose]
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker compose up -d --force-recreate --remove-orphans",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = vultr_instance.current.main_ip
+      user        = "root"
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+}
