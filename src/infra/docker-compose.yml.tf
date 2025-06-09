49df439b40f14e776fad2a9df4a7fac00d74636f
metairie <stephane.metairie@gmail.com>
2025-06-09 20:25:16 +0200

    add worker n8n

--- src/infra/docker-compose.yml.tf
+++ src/infra/docker-compose.yml.tf
@@ -5,6 +5,39 @@ data "template_file" "compose" {
       n8n_storage:
       redis_storage:
 
+    x-shared: &shared
+      restart: always
+      image: docker.n8n.io/n8nio/n8n
+      environment:
+        - DB_TYPE=postgresdb
+        - DB_POSTGRESDB_HOST=postgres
+        - DB_POSTGRESDB_PORT=5432
+        - DB_POSTGRESDB_DATABASE=${var.postgres_db}
+        - DB_POSTGRESDB_USER=${var.postgres_non_root_user}
+        - DB_POSTGRESDB_PASSWORD="${var.postgres_non_root_password}"
+        - EXECUTIONS_MODE=queue
+        - QUEUE_BULL_REDIS_HOST=redis
+        - QUEUE_HEALTH_CHECK_ACTIVE=true
+        - N8N_ENCRYPTION_KEY="${var.encryption_key}"
+        - N8N_BASIC_AUTH_ACTIVE=true
+        - N8N_BASIC_AUTH_USER=${var.n8n_user}
+        - N8N_BASIC_AUTH_PASSWORD="${var.n8n_password}"
+      links:
+        - postgres
+        - redis
+      volumes:
+        - n8n_storage:/home/node/.n8n
+      depends_on:
+        redis:
+          condition: service_healthy
+        postgres:
+          condition: service_healthy
+      healthcheck:
+        test: ["CMD", "curl", "-f", "http://localhost:5678/healthz"]
+        interval: 10s
+        timeout: 5s
+        retries: 5
+
     services:       
       postgres:
         image: postgres:16
@@ -36,40 +69,16 @@ data "template_file" "compose" {
           retries: 10
 
       n8n:
-        restart: always
-        image: docker.n8n.io/n8nio/n8n
-        environment:
-          - DB_TYPE=postgresdb
-          - DB_POSTGRESDB_HOST=postgres
-          - DB_POSTGRESDB_PORT=5432
-          - DB_POSTGRESDB_DATABASE=${var.postgres_db}
-          - DB_POSTGRESDB_USER=${var.postgres_non_root_user}
-          - DB_POSTGRESDB_PASSWORD="${var.postgres_non_root_password}"
-          - EXECUTIONS_MODE=queue
-          - QUEUE_BULL_REDIS_HOST=redis
-          - QUEUE_HEALTH_CHECK_ACTIVE=true
-          - N8N_ENCRYPTION_KEY="${var.encryption_key}"
-          - N8N_BASIC_AUTH_ACTIVE=true
-          - N8N_BASIC_AUTH_USER=${var.n8n_user}
-          - N8N_BASIC_AUTH_PASSWORD="${var.n8n_password}"
-        links:
-          - postgres
-          - redis
-        volumes:
-          - n8n_storage:/home/node/.n8n
-        depends_on:
-          redis:
-            condition: service_healthy
-          postgres:
-            condition: service_healthy
+        <<: *shared
         ports:
           - 5678:5678
-        healthcheck:
-          test: ["CMD", "curl", "-f", "http://localhost:5678/healthz"]
-          interval: 10s
-          timeout: 5s
-          retries: 5
 
+      n8n-worker:
+        <<: *shared
+        command: worker
+        depends_on:
+          - n8n
+        
       nginx:
         image: nginx:alpine
         container_name: nginx
